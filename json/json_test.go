2c4d7c44ca38c6ae4ac2da3fa3fb47ca39009544
guonaihong <guonaihong@qq.com>
2023-01-20 23:38:23 +0800

    更新

--- json/json_test.go
+++ json/json_test.go
@@ -3,7 +3,9 @@
 package json
 
 import (
+	"bytes"
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -17,9 +19,32 @@ func Test_Gen_Obj_JSON(t *testing.T) {
    "third" : [{"b1" : "b1", "b2" : "b2"}]
 }
   `
-	j, err := New([]byte(obj), "reqName", "json")
+	j, err := New([]byte(obj), WithStructName("reqName"), WithTagName("json"))
 	assert.NoError(t, err)
 	all, err := j.Marshal()
 	assert.NoError(t, err)
 	fmt.Println(string(all))
+
+	need, err := os.ReadFile("../testdata/test1.txt")
+	assert.NoError(t, err)
+	assert.Equal(t, string(bytes.TrimSpace(need)), string(all))
+}
+
+func Test_Gen_Obj_JSON2(t *testing.T) {
+	obj := `
+{
+   "first" : ["a", "b"],
+   "second" : {"b1" : "b1", "b2" : "b2"},
+   "third" : [{"b1" : "b1", "b2" : "b2"}]
+}
+  `
+	j, err := New([]byte(obj), WithStructName("reqName"), WithTagName("json"), WithNotInline())
+	assert.NoError(t, err)
+	all, err := j.Marshal()
+	assert.NoError(t, err)
+	fmt.Println(string(all))
+
+	need, err := os.ReadFile("../testdata/test1.txt")
+	assert.NoError(t, err)
+	assert.Equal(t, string(bytes.TrimSpace(need)), string(all))
 }
